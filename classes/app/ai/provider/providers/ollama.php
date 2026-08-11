bee288c7a2267e92c4c4409879d27dd658a64bec
Mathias Hjorth <mhj@moxis.dk>
2026-08-11 09:26:06 +0200

    #https://github.com/praxisdigital/moodle-opgaver/issues/1055 - Added merger function to ollama and mistral

--- classes/app/ai/provider/providers/ollama.php
+++ classes/app/ai/provider/providers/ollama.php
@@ -143,6 +143,8 @@ class ollama extends provider implements interfaces\chat_completion, interfaces\
             throw new invalid_provider_instance_configuration('Chat model is not configured');
         }
 
+        $messages = $this->merge_system_messages($messages);
+
         $payload = [
             'model' => $this->chat_model,
             'stream' => false,
